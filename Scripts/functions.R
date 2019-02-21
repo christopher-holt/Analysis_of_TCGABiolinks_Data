5b6d681b9bcaf48660180db0365b96a6d85a0982
Chris Holt <chrisholt97@gmail.com>
2019-02-21 11:34:30 -0500

    started to edit functions script to remove sites with no data

--- Scripts/functions.R
+++ Scripts/functions.R
@@ -119,10 +119,6 @@ merge_ind <- function(clin, mut){
 sites <- function(Data, category){
   pot_sites <- as.character(unique(Data$tissue_or_organ_of_origin))
   cat <- as.character(unique(Data$category))
-}
-
-pot_sites <- (unique(HNSC$tissue_or_organ_of_origin))
-for(i in 1:length(pot_sites)){
   set1 <- Data %>% filter(mutation_status == "Somatic",
                           variant_classification %in% c("Missense_Mutation", 
                                                         "Nonsense_Mutations", "Silent",
@@ -130,8 +126,26 @@ for(i in 1:length(pot_sites)){
                                                         "Frame_Shift_Ins", "In_Frame_Del", 
                                                         "In_Frame_Ins", "Indel"), 
                           tissue_or_organ_of_origin == pot_sites[i])
+}
+
+pot_sites <- (unique(HNSC$tissue_or_organ_of_origin))
+for(i in 1:length(pot_sites)){
+  set1 <- HNSC %>% filter(mutation_status == "Somatic",
+                          variant_classification %in% c("Missense_Mutation", 
+                                                        "Nonsense_Mutations", "Silent",
+                                                        "Frame_Shift_Del",
+                                                        "Frame_Shift_Ins", "In_Frame_Del", 
+                                                        "In_Frame_Ins", "Indel"), 
+                          tissue_or_organ_of_origin == pot_sites[i])
   
+  set1_a <- set1 %>% filter(race == "white")
+  set1_b <- set1 %>% filter(race == "black or african american")
   
+  if((nrow(set1_b) == 0 | nrow(set1_a) == 0)){
+    pot_sites <- pot_sites[pot_sites != pot_sites[i]]
+  } else if(length(unique(set1_b$tumor_barcode < 5)) | length(unique(set1_a$tumor_barcode) < 5)){
+              pot_sites <- pot_sites[pot_sites != pot_sites[i]]
+  }
 }
 
 ##-----------------------------------------------------------------------------------------------------------
