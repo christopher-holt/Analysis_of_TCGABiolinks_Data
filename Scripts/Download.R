9eb26a8c72c70d81e34cc25353b28f69fed815d5
Chris Holt <chrisholt97@gmail.com>
2019-02-21 00:47:48 -0500

    Edits made to code

--- Scripts/Download.R
+++ Scripts/Download.R
@@ -4,7 +4,7 @@
 
 library(tidyverse)
 library(TCGAbiolinks)
-source("functions.R")
+source("Scripts/functions.R")
 ##-----------------------------------------------------------------------------------------------------------
 ## set working directory to datasets directory
 ##-----------------------------------------------------------------------------------------------------------
@@ -16,16 +16,46 @@ setwd('Datasets')
 ## and occurence
 ##-----------------------------------------------------------------------------------------------------------
 
-Cancers <- c("LAML", "BLCA", "COAD", "ESCA","KICH", "KIRC", "KIRP", "LUAD", "LUSC", "PAAD", "STAD", "LIHC" )
-Cancers <- "HNSC"
+Cancers <- c("LAML", "BLCA")
+Cancers1 <- c("COAD", "ESCA")
+Cancers2 <- c("KICH", "KIRC")
+Cancers3 <- c("KIRP", "LUAD")
+Cancers4 <- c("LUSC", "PAAD")
+Cancers5 <- c("STAD", "LIHC" )
+
 ##-----------------------------------------------------------------------------------------------------------
 ## This will utilise the created merged function from functions.R to download and merge
 ## this clinical and mutational data from TCGA biolinks for each cancer
 ##-----------------------------------------------------------------------------------------------------------
-
 for (i in 1:length(Cancers)){
   total <- merged(Cancers[i])
-  assign(paste(Cancers[i], "1", sep = "_"), total)
+  write.csv(total, paste0(Cancers[i], ".csv"))
+  assign(paste(Cancers[i]), total)
+}
+for (i in 1:length(Cancers1)){
+  total <- merged(Cancers1[i])
+  write.csv(total, paste0(Cancers1[i], ".csv"))
+  assign(paste(Cancers1[i]), total)
+}
+for (i in 1:length(Cancers2)){
+  total <- merged(Cancers2[i])
+  write.csv(total, paste0(Cancers2[i], ".csv"))
+  assign(paste(Cancers2[i]), total)
 }
 
-
+for (i in 1:length(Cancers3)){
+  total <- merged(Cancers3[i])
+  write.csv(total, paste0(Cancers3[i], ".csv"))
+  assign(paste(Cancers3[i]), total)
+}
+for (i in 1:length(Cancers4)){
+  total <- merged(Cancers4[i])
+  write.csv(total, paste0(Cancers4[i], ".csv"))
+  assign(paste(Cancers4[i]), total)
+}
+for (i in 1:length(Cancers5)){
+  total <- merged(Cancers5[i])
+  write.csv(total, paste0(Cancers5[i], ".csv"))
+  assign(paste(Cancers5[i]), total)
+}
+rm(total)
