201801a1f8db67d9bcf5ff835b7dce99f9ed8c44
Chris Holt <chrisholt97@gmail.com>
2019-02-26 17:10:48 -0500

    Fixed sites script that was originally in here and moved it to functions.R

--- Scripts/functionsTEST.R
+++ Scripts/functionsTEST.R
@@ -7,40 +7,52 @@ library(magrittr)
 sites <- function(Data, category){
   pot_sites <- as.character(unique(Data$tissue_or_organ_of_origin))
   new_site <- character()
-  
-  if ("white" %in% Data$category){
-    Data %<>% filter(race %in% c("white", "black or african american"))
-    cats <- as.character(unique(Data$category))
-  } else if(category == "gender"){
-    cats <- as.character(unique(Data$category))
-  }
-  
-  for (i in 1:length(pot_sites)){
-    set1 <- Data %>% filter(mutation_status == "Somatic",
-                            variant_classification %in% c("Missense_Mutation", 
-                                                          "Nonsense_Mutations", "Silent",
-                                                          "Frame_Shift_Del",
-                                                          "Frame_Shift_Ins", "In_Frame_Del", 
-                                                          "In_Frame_Ins", "Indel"), 
-                            tissue_or_organ_of_origin == pot_sites[i])
-    
-    set1_a <- set1 %>% filter(category == cats[1])
-    set1_b <- set1 %>% filter(category == cats[2])
-    
-    if((nrow(set1_b) > 0 & nrow(set1_a) > 0) & ((length(unique(set1_b$tumor_barcode)) >= 3 & length(unique(set1_a$tumor_barcode)) >= 3))){
-      new_site <- c(new_site, pot_sites[i])
-    } 
-    
+  category <- as.name(paste0(category))
+  if ("white" %in% Data[[category]]){
+    try <- Data%>% filter(race %in% c("white", "black or african american"))
+    pot_sites <- as.character(unique(try$tissue_or_organ_of_origin))
+    cats <- as.character(unique(try[[category]]))
+    for (i in 1:length(pot_sites)){
+      set1 <- try %>% filter(mutation_status == "Somatic",
+                             variant_classification %in% c("Missense_Mutation", 
+                                                           "Nonsense_Mutations", "Silent",
+                                                           "Frame_Shift_Del",
+                                                           "Frame_Shift_Ins", "In_Frame_Del", 
+                                                           "In_Frame_Ins", "Indel"), 
+                             tissue_or_organ_of_origin == pot_sites[i])
+      
+      set1_a <- set1 %>% filter(race == cats[1])
+      set1_b <- set1 %>% filter(race == cats[2])
+      
+      if((nrow(set1_b) > 0 & nrow(set1_a) > 0) & ((length(unique(set1_b$tumor_barcode)) >= 3 & length(unique(set1_a$tumor_barcode)) >= 3))){
+        new_site <- c(new_site, pot_sites[i])
+      } 
+    }
+    message("category does not equal gender")
+  } else {
+    cats <- as.character(unique(Data[[category]]))
+    for (i in 1:length(pot_sites)){
+      set1 <- try %>% filter(mutation_status == "Somatic",
+                             variant_classification %in% c("Missense_Mutation", 
+                                                           "Nonsense_Mutations", "Silent",
+                                                           "Frame_Shift_Del",
+                                                           "Frame_Shift_Ins", "In_Frame_Del", 
+                                                           "In_Frame_Ins", "Indel"), 
+                             tissue_or_organ_of_origin == pot_sites[i])
+      
+      set1_a <- set1 %>% filter(gender == cats[1])
+      set1_b <- set1 %>% filter(gender == cats[2])
+      
+      if((nrow(set1_b) > 0 & nrow(set1_a) > 0) & ((length(unique(set1_b$tumor_barcode)) >= 3 & length(unique(set1_a$tumor_barcode)) >= 3))){
+        new_site <- c(new_site, pot_sites[i])
+      } 
+    }
   }
   return(new_site)
 }
 ##-----------------------------------------------------------------------------------------------------------
 ## Testing sites function
 ##-----------------------------------------------------------------------------------------------------------
-
-a <- sites(HNSC, race)
-b <- sites(HNSC, gender)
-
 
 ##-----------------------------------------------------------------------------------------------------------
 ## End of Script
