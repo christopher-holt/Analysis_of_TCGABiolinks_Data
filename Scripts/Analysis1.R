2c67205c78bd2b417169cf2b07a4c6e9c0afef9b
Chris Holt <chrisholt97@gmail.com>
2019-02-26 22:00:35 -0500

    Updated file so that it will generate pValue/quartile info for each cancer, location, and pipeline for smokers vs nonsmokers

--- Scripts/Analysis1.R
+++ Scripts/Analysis1.R
@@ -1,60 +1,68 @@
 ##-----------------------------------------------------------------------------------------------------------
+## This file will compare smoker vs nonsmoker data from 12 cancer datasets
+## In this script, non smokers are defined as all participants who have NA for cigarettes per day and
+## Smokers are all patients who have a value for cigrattes per day
+##-----------------------------------------------------------------------------------------------------------
+
+##-----------------------------------------------------------------------------------------------------------
 ## imports
 ##-----------------------------------------------------------------------------------------------------------
 
-library(tidyverse)
-library(data.table)
+source("../Scripts/functions.R")
 setwd("~/Research/BiolinksAnalysis/Datasets")
 ##-----------------------------------------------------------------------------------------------------------
 ## Read in the data
 ##-----------------------------------------------------------------------------------------------------------
-Data_Names <- c("LAML", "HNSC")#, "BLCA", "ESCA", "KICH", "KIRC", "LUSC", "PAAD", "STAD", "LIHC", "KIRP", "LUAD")
+Data_Names <- c("BLCA", "HNSC")#, "ESCA", "KICH", "KIRC", "LUSC", "PAAD", "STAD", "LIHC", "KIRP", "LUAD")
 
+## LAML was removed for having no smoking data
+
+pipelines <- unique(HNSC$pipeline)
 ## This will read in each Cancer and determine the important sites
 for (i in 1:length(Data_Names)){
   df <- read_delim(paste0(Data_Names[i], "_select.csv"), delim = "\t")
-  assign(paste(Data_Names[i]), df)
-  rm(df)
+  df <- df %>% filter(mutation_status == "Somatic",
+                             variant_classification %in% c("Missense_Mutation", 
+                                                           "Nonsense_Mutations", "Silent",
+                                                           "Frame_Shift_Del",
+                                                           "Frame_Shift_Ins", "In_Frame_Del", 
+                                                           "In_Frame_Ins", "Indel")) 
+  valid_sites <- smoke_sites(df)
+  for(j in 1:length(pipelines)){
+    for(k in 1:length(valid_sites)){
+    ## Subset the data into smokers and non-smokers. These will be in a loop that separates cancer, pipeline, and location
+    ## Set_1 are non-smokers, Set_2 are smokers
+      Set_1 <- df %>% filter(is.na(cigarettes_per_day), pipeline == pipelines[j], tissue_or_organ_of_origin == valid_sites[k])
+      Set_2 <- df %>% filter(!(is.na(cigarettes_per_day)), pipeline == pipelines[j], tissue_or_organ_of_origin == valid_sites[k])
+    
+    ## For each dataset, find the total number of somatic point mutations per person 
+      Set_2_Sum <- number_mut(Set_2, "smoker")
+      Set_1_Sum <- number_mut(Set_1, "non-smoker")
+      Sum <- rbind(Set_2_Sum, Set_1_Sum)
+      total_mut <- ggplot(Sum) + geom_boxplot(aes(x = status, y = total))
+
+    ## Calculate the pValue and quartile data for both groups
+      pVal_Sum <- wilcox.test(total ~ status, data = Sum, paired = F)$p.value
+      Overview_Set_2 <- summary_data(Set_2_Sum)
+      Overview_Set_1 <- summary_data(Set_1_Sum)
+
+    ## Combine summary data
+      Overview_Set_2$status <- "smoker"
+      Overview_Set_1$status <- "non-smoker"
+      Overview <- rbind(Overview_Set_2, Overview_Set_1)
+      Overview$pValue <- pVal_Sum
+    
+    ## Save summary data and plots
+      write_delim(Overview, file.path("../Output/Smoke/total_mut_pval/", paste0(pipelines[j], "/", 
+                                                                                Data_Names[i],"_", valid_sites[k], 
+                                                                                ".csv")), delim  = "\t")
+      ggsave(total_mut, file = file.path("../Output/Smoke/total_mut_graphs/", paste0(pipelines[j], "/", 
+                                                                                     Data_Names[i], "_", valid_sites[k], 
+                                                                                     ".jpg")), width = 6,
+             height = 6, units = "in")
+    }
+  }
 }
-
-##-----------------------------------------------------------------------------------------------------------
-## Subset the data into smokers and non-smokers. These will be in a loop that separates cancer, pipeline, and 
-## location
-##-----------------------------------------------------------------------------------------------------------
-not_smoke <- HNSC %>% filter(is.na(cigarettes_per_day))
-smoke <- HNSC %>% filter(!(is.na(cigarettes_per_day)))
-
-
-##-----------------------------------------------------------------------------------------------------------
-## For each dataset, find the total number of somatic point mutations per person 
-##-----------------------------------------------------------------------------------------------------------
-smoke_Sum <- number_mut(smoke, "smoker")
-not_smoke_Sum <- number_mut(not_smoke, "non-smoker")
-Sum <- rbind(smoke_Sum, not_smoke_Sum)
-
-
-
-ggplot(Sum) + geom_boxplot(aes(x = status, y = total)) + ylim(0,1000)
-
-pVal_Sum <- wilcox.test(total ~ status, data = Sum, paired = F)$p.value
-write.csv(pVal_Sum, file = file.path("/home/chris-holt/Research/BiolinksAnalysis/Output/Smoke/total_mut_pval/",
-                                     paste(cancers[i], '_', pipelines[j],'_', site[n],'_pvalue', 
-                                           '.csv')), row.names = F)
-
-  
-summary_smoke <- as.data.frame(do.call(cbind, lapply(smoke_Sum, summary)))
-summary_not_smoke <- as.data.frame(do.call(cbind, lapply(not_smoke_Sum, summary)))
-summary_smoke <- add_rownames(summary_smoke, "Values")
-summary_not_smoke <- add_rownames(summary_not_smoke, "Values")
-Overview <- summary_smoke %>% select(Values, total)
-Overview$pValue <- pVal_Sum
-  
-
-
-
-
-
-
 
 
 
