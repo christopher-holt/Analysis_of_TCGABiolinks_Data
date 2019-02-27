df619c00e2e44173891220b55d4f883168adb2b3
Chris Holt <chrisholt97@gmail.com>
2019-02-26 23:13:35 -0500

    General Update

--- Scripts/select_col.py
+++ Scripts/select_col.py
@@ -1,14 +1,27 @@
-#!/usr/bin/env python
-# coding: utf-8
 #!/usr/bin/python3
+"""
+This file will take flat files downloaded by Download.R and will extract important columns to create smaller, useable files
+to analyse
+
+"""
+
+##-----------------------------------------------------------------------------------------------------------
+## Imports
+##-----------------------------------------------------------------------------------------------------------
 import pandas
 import os
 import glob
+
+##-----------------------------------------------------------------------------------------------------------
+## change to directory containing datasets and add them to a list
+##-----------------------------------------------------------------------------------------------------------
 os.chdir("../Datasets")
 filenames = (glob.glob("*[!_select]*.csv")) 
-
+##-----------------------------------------------------------------------------------------------------------
+## for each file, extract certain columns and create new flat file "\t" deliminated
 ## For system memory purposes, it may be easier to run the script in jupyter and rerun the last cell as range(0,1) then range(1,2) etc through range(11,12) instead
 ## of one big loop
+##-----------------------------------------------------------------------------------------------------------
 for file in range(len(filenames)):
     df = pandas.read_csv(filenames[file], sep="\t", low_memory = False)
     df1 = df[["tumor_barcode", "primary_diagnosis", "tumor_stage", "gender", "ethnicity", "race", "age_at_diagnosis", 
