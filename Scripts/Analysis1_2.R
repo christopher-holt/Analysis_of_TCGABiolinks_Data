9725b9befa43f9d52c04ca479002821273fe9cdf
Chris Holt <chrisholt97@gmail.com>
2019-03-20 22:11:49 -0400

    This R file will find gene frequencies between smokers and none smokers

--- Scripts/Analysis1_2.R
+++ Scripts/Analysis1_2.R
@@ -45,7 +45,9 @@ for (i in 1:length(Data_Names)){
     Set_1 <- df %>% filter(is.na(cigarettes_per_day), pipeline == pipelines[j])
     Set_2 <- df %>% filter(!(is.na(cigarettes_per_day)), pipeline == pipelines[j])
     
-    
+    Set_1_people <- length(unique(Set_1$tumor_barcode))
+    Set_2_people <- length(unique(Set_2$tumor_barcode))
+ 
     genes_1 <- as.data.frame(gene_count(Set_1))
     genes_2 <- as.data.frame(gene_count(Set_2))
     
@@ -58,9 +60,56 @@ for (i in 1:length(Data_Names)){
     diff_1$n <- as.integer(0)
     diff_2$n <- as.integer(0)
     
+    num_people_1 <- genes_1 %>% select(hugo_symbol, tumor_barcode) %>% group_by(hugo_symbol) %>% count()
+    num_people_2 <- genes_2 %>% select(hugo_symbol, tumor_barcode) %>% group_by(hugo_symbol) %>% count()
+    
+    genes_1 <- genes_1 %>% select(hugo_symbol, n)
+    genes_2 <- genes_2 %>% select(hugo_symbol, n)
+    
     genes_1 <- rbind(genes_1, diff_2)
     genes_2 <- rbind(genes_2, diff_1)
-
+    
+    num_same_genes <- nrow(same_genes)
+    num_1_genes <- genes_1 %>% filter(n > 0) %>% nrow()
+    num_2_genes <- genes_2 %>% filter(n > 0) %>% nrow()
+    
+    
+    genes_1$status <- "NS"
+    genes_2$status <- "S"
+    
+    genes_1 <- genes_1 %>% left_join(num_people_1, by = "hugo_symbol")
+    genes_2 <- genes_2 %>% left_join(num_people_2, by = "hugo_symbol")
+    genes_1$n.y[is.na(genes_1$n.y)] <- 0
+    genes_2$n.y[is.na(genes_2$n.y)] <- 0
+    
+    
+    genes_1 <- genes_1 %>% group_by(hugo_symbol) %>% mutate(perc = (n.x/n.y)*100)
+    genes_2 <- genes_2 %>% group_by(hugo_symbol) %>% mutate(perc = (n.x/n.y)*100)
+    
+    genes <- rbind(genes_1, genes_2)
+    genes$perc[is.nan(genes$perc)] <- 0
+    
+    setwd("~/Research/BiolinksAnalysis/Output/Smoke/Genes_Pvalues")
+    write_delim(genes, paste0(pipelines[j], "_", Data_Names[i], "_Gene_pvalues", ".tsv"), delim = "\t")
+    setwd("~/Research/BiolinksAnalysis/")
+    
+    # 
+    # uniq_genes <- as.character(unique(genes$hugo_symbol))
+    # 
+    # genes_pval <- data.frame()
+    # data <- tibble("hugo_symbol", "pval")
+    # for(k in 1:length(uniq_genes)){
+    #   gene_data <- genes %>% filter(hugo_symbol == uniq_genes[k])
+    #   pVal <- wilcox.test(perc ~ status, data = gene_data, paired = F)$p.value ##Keep on getting pValues of 1
+    #   
+    #   
+    #   data$hugo_symbol <- uniq_genes[k]
+    #   data$pVal <- pVal
+    #   
+    #   genes_pval <- rbind(genes_pval, data)
+    #   genes_pval$pVal[is.nan(genes_pval$pVal)] <- 1.1
+    # }
+    # genes_pavl <- genes_pval %>% select(hugo_symbol, pVal)
   }
 }
 
