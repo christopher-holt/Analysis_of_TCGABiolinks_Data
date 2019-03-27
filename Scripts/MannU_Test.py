488440d1d3403cea6c5f3970a89c7b8089d22ce2
Chris Holt <chrisholt97@gmail.com>
2019-03-27 11:18:18 -0400

    Updated to find pvlaues for genes only from somatic sniper pipeline

--- Scripts/MannU_Test.py
+++ Scripts/MannU_Test.py
@@ -8,31 +8,34 @@ This file will take flat files containing frequencies of genes and calculate the
 
 Date Created: 15/Mar/2019
 
-Date of Last Update: 26/Mar/2019
+Date of Last Update: 27/Mar/2019
 
 """
 
 ## Imports
 import pandas
+import time
 from scipy import stats
 import os
 import glob
 
 
 ## Function that will calculate pValues 
-def pvalues(string, disease):
+def pvalues(string, pipeline):
+    print(time.ctime())
     os.chdir("/home/chris-holt/Research/BiolinksAnalysis/Output/%s/Genes_Pvalues" %(str(string)))
-    filenames = glob.glob("*_%s*[!_FINAL_PVALUES]*.tsv" % disease)
+    filenames = glob.glob("%s_*[!_FINAL_PVALUES]*.tsv" % pipeline)
     for file in range(len(filenames)):
         df = pandas.read_table(filenames[file], sep = "\t", low_memory = False)
         genes = df["hugo_symbol"].unique()
         final_df = pandas.DataFrame(columns = ["gene", "pvalue"])
+        stat = df.status.unique()
         for k in range(len(genes)):
             try:
                 df1 = df.loc[df.hugo_symbol == genes[k]]
-                df1_1 = df1.loc[df1.status == "NS"]
-                df1_2 = df1.loc[df1.status == "S"]
-                pval = stats.mannwhitneyu(df1_1["perc"], df1_2["perc"])
+                df1_1 = df1.loc[df1.status == stat[0]]
+                df1_2 = df1.loc[df1.status == stat[1]]
+                pval = stats.mannwhitneyu(df1_1["n.x"], df1_2["n.x"])
                 data = [[genes[k], pval.pvalue]]
                 b = pandas.DataFrame(data, columns = ["gene","pvalue"])
                 final_df = pandas.concat([final_df, b])
@@ -42,34 +45,20 @@ def pvalues(string, disease):
         name = str(filenames[file].split("_")[1]+ "_" + filenames[file].split("_")[0] + "_FINAL_PVALUES")
         final_df.to_csv(name, sep = "\t")
         print(name + " download complete")
-    
+        print(time.ctime())
  
 def main():
     
     ## I have broken down the process to be easier on the computer processor
     ## Enter in a valid directory and cancer cohort
-    group = str(input("Please enter a directory (Smoke/Race/Gender): " ))
-    Cancer = str(input("Please enter a Cancer: "))
-    pvalues(group, Cancer)
+#    group = str(input("Please enter a directory (Smoke/Race/Gender): " ))
+#    Cancer = str(input("Please enter a Cancer: "))
+#    pvalues(group, Cancer)
+    pvalues("Smoke", "somaticsniper")
+    pvalues("Race", "somaticsniper")
+    pvalues("Gender", "somaticsniper")
     
-    ## Valid directories:
-        ## Smoke
-        ## Race
-        ## Gender
-    ## Valid Cohorts:
-        ## BLCA 
-        ## HNSC
-        ## KICH
-        ## KIRP
-        ## LUAD
-        ## LUSC
-        ## PAAD
-    ## (Race/Gender Only)
-        ## KIRC, LIHC, STAD
-
-## Takes ~15min to run for one combination
-
-## Completed combos(Smoke:BLCA)
+    ## initially looking at somatic sniper pipelines 
 
 
 main()
