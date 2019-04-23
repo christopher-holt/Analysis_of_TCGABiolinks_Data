677e2c6f0763258baa3e817fbcaa761e5967a51a
Chris Holt <chrisholt97@gmail.com>
2019-04-23 13:08:59 -0400

    Updated to improve download efficiency

--- Scripts/Download.R
+++ Scripts/Download.R
@@ -1,7 +1,7 @@
 ## Author: Chris Holt
 ## Purpose: To download and standardise TCGAbiolinks data
 ## Date Created: Feb/2019
-## Date of Last Update: 26/Mar/2019
+## Date of Last Update: 23/Apr/2019
 
 ##-----------------------------------------------------------------------------------------------------------
 ## imports
@@ -32,13 +32,7 @@ total_Cancers <- total_Cancers %>% pull()
 
 ## A select number of cancers
 
-Cancers <- c("LAML", "BLCA")
-Cancers1 <- c("ESCA","KICH")
-Cancers2 <- c("KIRC","KIRP")
-Cancers3 <- c("LUAD","LUSC")
-Cancers4 <- c("PAAD","STAD")
-Cancers5 <- c("LIHC" )
-
+Cancers <- c("LAML", "BLCA", "ESCA","KICH", "KIRC","KIRP", "LUAD","LUSC", "PAAD","STAD", "LIHC")
 ##-----------------------------------------------------------------------------------------------------------
 ## This will utilise the created merged function from functions.R to download and merge
 ## this clinical and mutational data from TCGA biolinks for each cancer
@@ -52,69 +46,8 @@ for (i in 1:length(Cancers)){
   total_sub <- total %>% select(one_of(columnsofInterest))
   write_delim(total_sub, paste0(Cancers[i], "_select.csv"), delim = "\t")
   #write_delim(total, paste0(Cancers[i], ".csv"), delim = "\t")
-  assign(paste(Cancers[i]), total)
-  assign(paste(Cancers[i], "_select"), total)
 }
-for (i in 1:length(Cancers1)){
-  clin <- download_clinical(Cancers1[i])
-  mut <- download_mutational(Cancers1[i])
-  mut <- classify_Changes(mut)
-  total <- inner_join(clin,mut, by = "tumor_barcode")
-  total_sub <- total %>% select(one_of(columnsofInterest))
-  write_delim(total_sub, paste0(Cancers1[i], "_select.csv"), delim = "\t")
-  #write_delim(total, paste0(Cancers1[i], ".csv"), delim = "\t")
-  assign(paste(Cancers1[i]), total)
-  assign(paste(Cancers1[i], "_select"), total)
-  
-}
-for (i in 1:length(Cancers2)){
-  clin <- download_clinical(Cancers2[i])
-  mut <- download_mutational(Cancers2[i])
-  mut <- classify_Changes(mut)
-  total <- inner_join(clin,mut, by = "tumor_barcode")
-  total_sub <- total %>% select(one_of(columnsofInterest))
-  write_delim(total_sub, paste0(Cancers2[i], "_select.csv"), delim = "\t")
-  #write_delim(total, paste0(Cancers2[i], ".csv"), delim = "\t")
-  assign(paste(Cancers2[i]), total)
-  assign(paste(Cancers2[i], "_select"), total)
-  
-}
-for (i in 1:length(Cancers3)){
-  clin <- download_clinical(Cancers3[i])
-  mut <- download_mutational(Cancers3[i])
-  mut <- classify_Changes(mut)
-  total <- inner_join(clin,mut, by = "tumor_barcode")
-  total_sub <- total %>% select(one_of(columnsofInterest))
-  write_delim(total_sub, paste0(Cancers3[i], "_select.csv"), delim = "\t")
-  #write_delim(total, paste0(Cancers3[i], ".csv"), delim = "\t")
-  assign(paste(Cancers3[i]), total)
-  assign(paste(Cancers3[i], "_select"), total)
-  
-}
-for (i in 1:length(Cancers4)){
-  clin <- download_clinical(Cancers4[i])
-  mut <- download_mutational(Cancers4[i])
-  mut <- classify_Changes(mut)
-  total <- inner_join(clin,mut, by = "tumor_barcode")
-  total_sub <- total %>% select(one_of(columnsofInterest))
-  write_delim(total_sub, paste0(Cancers4[i], "_select.csv"), delim = "\t")
-  #write_delim(total, paste0(Cancers4[i], ".csv"), delim = "\t")
-  assign(paste(Cancers4[i]), total)
-  assign(paste(Cancers4[i], "_select"), total)
-  
-}
-for (i in 1:length(Cancers5)){
-  clin <- download_clinical(Cancers5[i])
-  mut <- download_mutational(Cancers5[i])
-  mut <- classify_Changes(mut)
-  total <- inner_join(clin,mut, by = "tumor_barcode")
-  total_sub <- total %>% select(one_of(columnsofInterest))
-  write_delim(total_sub, paste0(Cancers5[i], "_select.csv"), delim = "\t")
- # write_delim(total, paste0(Cancers5[i], ".csv"), delim = "\t")
-  assign(paste(Cancers5[i]), total)
-  assign(paste(Cancers5[i], "_select"), total)
-  
-}
+
 
 rm(clin)
 rm(mut)
